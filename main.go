c55388996549fd1bb47967afc375f8b4892f2fc6
Alex Roden <alex.broom-roden@oneiota.co.uk>
2025-03-03 16:14:36 +0000

    run create as part of compare

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ func main() {
 			fmt.Println("Error:", err)
 		}
 	case "compare":
+		if err := pkg.Create(); err != nil {
+			fmt.Println("Error:", err)
+		}
+
 		if err := pkg.Compare("benchmark.txt", "benchmark_new.txt"); err != nil {
 			fmt.Println("Error:", err)
 		}
